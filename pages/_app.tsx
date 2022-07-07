d52eb347a2cd2949fd94ba848e2e26004801f468
tagawa shouta <sho.webfe@gmail.com>
2022-07-08 02:21:37 +0900

    animatePresence設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppPropsWithLayout } from '../types/page';
+import { Fragment } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import '../styles/global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <Fragment>
+      {getLayout(
+        <AnimatePresence exitBeforeEnter>
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
+      )}
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
